dyasny/Mayastor

commit 42ba4b5a4f228a1f3fa3824ff72bd3095ce84918

chore: try to enable cross builds
Signed-off-by: Ana Hobden <operator@hoverbear.org>

nix/pkgs/mayastor/default.nix
@@ -2,7 +2,6 @@
 , clang
 , dockerTools
 , e2fsprogs
-, git
 , lib
 , libaio
 , libiscsi
@@ -19,8 +18,10 @@
 , sources
 , xfsprogs
 , utillinux
-, rustup
-, docker-compose
+, llvmPackages_11
+, targetPackages
+, buildPackages
+, targetPlatform
 }:
 let
   channel = import ../../lib/rust.nix { inherit sources; };
@@ -36,8 +37,7 @@ let
             lib.hasPrefix (toString (src + "/${allowedPrefix}")) path)
           allowedPrefixes)
       src;
-  version_drv = import ../../lib/version.nix { inherit lib stdenv git; };
-  version = builtins.readFile "${version_drv}";
+  version = (builtins.fromTOML (builtins.readFile ../../../mayastor/Cargo.toml)).package.version;
   src_list = [
     "Cargo.lock"
     "Cargo.toml"
@@ -63,17 +63,13 @@ let
     PROTOC = "${protobuf}/bin/protoc";
     PROTOC_INCLUDE = "${protobuf}/include";
 
-    # Before editing dependencies, consider:
-    # https://nixos.org/manual/nixpkgs/stable/#ssec-cross-dependency-implementation
-    # https://nixos.org/manual/nixpkgs/stable/#ssec-stdenv-dependencies
-    basePackages = [
-    ];
     nativeBuildInputs = [
-      clang
       pkg-config
+      protobuf
+      llvmPackages_11.clang
     ];
     buildInputs = [
-      llvmPackages.libclang
+      llvmPackages_11.libclang
       protobuf
       libaio
       libiscsi.lib

default.nix
@@ -1,3 +1,7 @@
+{
+    crossSystem ? null
+}:
+
 let
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs {
@@ -5,6 +9,7 @@ let
       (_: _: { inherit sources; })
       (import ./nix/mayastor-overlay.nix)
     ];
+    inherit crossSystem;
   };
 in
 pkgs

nix/pkgs/libspdk/default.nix
@@ -1,21 +1,35 @@
 { binutils
 , cunit
 , fetchFromGitHub
-, pkgconfig
+, pkg-config
 , lcov
 , libaio
 , libiscsi
+, libbpf
+, libelf
 , liburing
 , libuuid
+, libpcap
+, libbsd
+, libexecinfo
 , nasm
+, cmake
 , ninja
+, jansson
 , meson
 , ncurses
 , numactl
 , openssl
 , python3
-, stdenv 
-, system ? builtins.currentSystem
+, stdenv
+, libtool
+, yasm
+, targetPlatform
+, buildPlatform
+, buildPackages
+, llvmPackages_11
+, gcc
+, zlib
 }:
 let
   # Derivation attributes for production version of libspdk
@@ -34,12 +48,16 @@ let
     nativeBuildInputs = [
       meson
       ninja
-      pkgconfig
+      pkg-config
       python3
+      llvmPackages_11.clang
+      gcc
+      cmake
     ];
 
     buildInputs = [
       binutils
+      libtool
       libaio
       libiscsi.dev
       liburing
@@ -48,39 +66,65 @@ let
       ncurses
       numactl
       openssl
+      libpcap
+      libbsd
+      jansson
+      libbpf
+      libelf
+      libexecinfo
+      zlib
     ];
 
-    configureFlags = (if (system == "x86_64-linux") then 
+    configureFlags = (if (targetPlatform.config == "x86_64-unknown-linux-gnu") then
       [
         "--target-arch=nehalem"
+        "--with-crypto"
       ]
-    else if (system == "aarch64-linux") then
+    else if (targetPlatform.config == "aarch64-unknown-linux-gnu") then
       [
         "--target-arch=armv8-a+crypto"
       ]
     else
       []
-    ) ++ [
-      #"--with-crypto"
+    ) ++
+    (if (targetPlatform.config != buildPlatform.config) then [ "--cross-prefix=${targetPlatform.config}" ] else []) ++
+    [
       "--without-isal"
       "--with-iscsi-initiator"
       "--with-uring"
+      "--disable-examples"
+      "--disable-unit-tests"
+      "--disable-tests"
     ];
 
     enableParallelBuilding = true;
 
-    preConfigure = ''
-      substituteInPlace dpdk/config/defconfig_x86_64-native-linux-gcc --replace native default
-    '';
-
     configurePhase = ''
       patchShebangs ./. > /dev/null
-      ./configure $configureFlags
     '';
 
     hardeningDisable = [ "all" ];
 
-    buildPhase = ''
+    buildPhase = (if (targetPlatform.config == "x86_64-unknown-linux-gnu") then
+      ''
+        substituteInPlace dpdk/config/defconfig_x86_64-native-linux-gcc --replace native default
+        meson build dpdk
+      ''
+    else if (targetPlatform.config == "aarch64-unknown-linux-gnu") then
+      ''
+        substituteInPlace dpdk/config/defconfig_x86_64-native-linux-gcc --replace native default
+        substituteInPlace dpdk/config/arm/arm64_armv8_linux_gcc --replace aarch64-linux-gnu- aarch64-unknown-linux-gnu-
+        meson build dpdk --cross-file dpdk/config/arm/arm64_armv8_linux_gcc
+      ''
+    else
+      ""
+    ) + ''
+      ./configure ${builtins.concatStringsSep
+            " "
+            (builtins.filter
+                (opt: (builtins.match "--build=.*" opt) == null)
+                configureFlags)
+        }
       make -j`nproc`
       find . -type f -name 'libspdk_event_nvmf.a' -delete
       find . -type f -name 'libspdk_sock_uring.a' -delete
@@ -122,7 +166,7 @@ let
   };
 in
 {
-  release = stdenv.mkDerivation (drvAttrs // {
+  release = llvmPackages_11.stdenv.mkDerivation (drvAttrs // {
     pname = "libspdk";
     separateDebugInfo = true;
     dontStrip = false;
@@ -131,7 +175,7 @@ in
       "--disable-unit-tests"
     ];
   });
-  debug = stdenv.mkDerivation (drvAttrs // {
+  debug = llvmPackages_11.stdenv.mkDerivation (drvAttrs // {
     pname = "libspdk-dev";
     separateDebugInfo = false;
     dontStrip = true;

commit 3b980ebe80910c23fe133fdaec46aa48a0d4b358

fix: use clang-11 consistently
In 42ba4b5a4 or #671 llvmPackages_11 was added to the build,
presumably to help with cross compiling. However, the inclusion of
x86intrin.h via nvme_internal.h when building spdk-sys resulted in
"invalid conversion" errors in the clang headers when running nix
build to build the mayastor image, but not in the regular nix-shell
environment.

That may be due to the use of clang-11 headers with clang-7 libraries
so use clang_11 and llvmPackages_11 throughout, update LIBCLANG_PATH,
and drop llvmPackages.

Fixes: 42ba4b5a4 ("chore: try to enable cross builds")

nix/pkgs/mayastor/cargo-package.nix
@@ -1,5 +1,5 @@
 { stdenv
-, clang
+, clang_11
 , dockerTools
 , e2fsprogs
 , lib
@@ -9,7 +9,6 @@
 , libspdk-dev
 , libudev
 , liburing
-, llvmPackages
 , makeRustPlatform
 , numactl
 , openssl
@@ -60,7 +59,7 @@ let
     cargoSha256 = "1rqp5w0148sajcdwg6dafbknsi62zkmd6i8m2c5zfghdlg4ripxc";
     inherit version cargoBuildFlags;
     src = whitelistSource ../../../. src_list;
-    LIBCLANG_PATH = "${llvmPackages.libclang}/lib";
+    LIBCLANG_PATH = "${llvmPackages_11.libclang}/lib";
     PROTOC = "${protobuf}/bin/protoc";
     PROTOC_INCLUDE = "${protobuf}/include";
 

nix/pkgs/mayastor/default.nix
@@ -1,5 +1,5 @@
 { stdenv
-, clang
+, clang_11
 , dockerTools
 , e2fsprogs
 , lib
@@ -9,7 +9,6 @@
 , libspdk-dev
 , libudev
 , liburing
-, llvmPackages
 , makeRustPlatform
 , numactl
 , openssl

nix/pkgs/mayastor/units.nix
@@ -1,5 +1,5 @@
 { stdenv
-, clang
+, clang_11
 , dockerTools
 , e2fsprogs
 , lib
@@ -9,7 +9,6 @@
 , libspdk-dev
 , libudev
 , liburing
-, llvmPackages
 , makeRustPlatform
 , numactl
 , openssl

shell.nix
@@ -25,7 +25,7 @@ mkShell {
     kubectl
     kind
     docker
-    clang
+    clang_11
     cowsay
     e2fsprogs
     fio
@@ -40,7 +40,7 @@ mkShell {
     libiscsi
     libudev
     liburing
-    llvmPackages.libclang
+    llvmPackages_11.libclang
     nats-server
     nodejs-12_x
     numactl